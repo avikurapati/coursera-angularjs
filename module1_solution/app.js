df4462fc19dcf00e9606204ed984bb6d4ad910cf
Avinash Kurapati <avikurapati@gmail.com>
2017-01-31 22:40:04 -0800

    Solution for module1 assignment

---
+++ module1_solution/app.js
@@ -0,0 +1,28 @@
+'use strict';
+
+(function() {
+
+	angular.module('LunchCheck', [])
+		.controller('LunchCheckController', LunchCheckController);
+
+	LunchCheckController.$inject = ['$scope'];
+
+	function LunchCheckController($scope) {
+
+		$scope.remarks = '';
+		$scope.checkLunchItems = function() {
+			var lunchItemsArray = $scope.lunchItems.split(',');
+			lunchItemsArray = _.filter(lunchItemsArray, function(lunchItem) {
+				return lunchItem.trim() != '';
+			})
+			if (lunchItemsArray.length > 3) {
+				$scope.remarks = "Too much!";
+			} else if (lunchItemsArray.length === 0) {
+				$scope.remarks = "Please enter data first";
+			} else {
+				$scope.remarks = "Enjoy!";
+			}
+		};
+	}
+
+})();
